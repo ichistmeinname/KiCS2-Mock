699c057a09ae3308b4f9b8cd483f5ddd03a3d8b5
Sandra Dylus <sad@informatik.uni-kiel.de>
2015-08-11 10:27:23 +0200

    Add more examples

--- TestMock.hs
+++ TestMock.hs
@@ -12,7 +12,10 @@ genBool n = do
 boolsSmall = do
   s <- initSupply
   let [id1,id2] = map unique (nextSupplies 2 s)
-  return $ Choice_C_Bool 1 (ChoiceID id1) C_True (Choice_C_Bool 2 (ChoiceID id2) undefined C_True)
+  return $ Choice_C_Bool 1
+                         (ChoiceID id1)
+                         C_True
+                         (Choice_C_Bool 2 (ChoiceID id2) undefined C_True)
 
 runMock' val = dfsStrategy (runMock val)
 
@@ -26,10 +29,17 @@ fold'    = foldValues d_OP_amp_amp C_True . runValues
 isEmpty' :: NormalForm a => a -> Bool
 isEmpty' = isEmpty . runValues
 
-foldValues :: (NormalForm a, NormalForm b) => (a -> b -> b) -> b -> C_Values a -> b
+foldValues :: (NormalForm a, NormalForm b)
+           => (a -> b -> b)
+           -> b
+           -> C_Values a -> b
 foldValues f z (C_Values cList) = d_C_foldr f z cList
 
-foldSequence :: (NormalForm a, NormalForm b) => (a -> b -> b) -> b -> C_ValueSequence a -> b
+foldSequence :: (NormalForm a, NormalForm b)
+             => (a -> b -> b)
+             -> b
+             -> C_ValueSequence a
+             -> b
 foldSequence f z (Values cList) = d_C_foldr f z cList
 
 isEmpty :: C_Values a -> Bool
@@ -46,6 +56,15 @@ isEmptySequence _       = False
 
 data C_Values a = C_Values (OP_List a)
   deriving Show
+
+sharingExample = do
+  s <- initSupply
+  let (id1:id2:ids) = map unique (nextSupplies 100002 s)
+      x = Choice_C_Bool 2 (ChoiceID id2) C_False C_True
+      list = foldr (\(x,y) acc -> Choice_C_Bool y (ChoiceID x) C_True acc)
+                   x
+                   (zip ids [3..])
+  return (Choice_C_Bool 1 (ChoiceID id1) x list)
 
 main :: IO ()
 main = genBool 100000 >>= print . fold'
